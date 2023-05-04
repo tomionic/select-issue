ef2f9a0d84d8dfdafecd96581af2231300603e12
Tom Driver <tom@Toms-MacBook-Pro.local>
2023-05-04 19:44:20 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,12 @@
+import { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'select-issue',
+  webDir: 'www',
+  server: {
+    androidScheme: 'https'
+  }
+};
+
+export default config;
